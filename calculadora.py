39381dfd22b77c58bf356b62857a8d58b87746ba
VidaLoka0001 <77423328+VidaLoka0001@users.noreply.github.com>
2021-01-14 00:05:29 -0300

    Add files via upload

---
+++ calculadora.py
@@ -0,0 +1,14 @@
+n0 = str(input('Qual é o seu nome ? '))
+print('Olá',n0,'!')
+n1 = int(input('Digite um valor: '))
+n2 = int(input('Agora digite outro valor: '))
+S = n1 + n2
+SB = n1 - n2
+M = n1 * n2
+D = n1 / n2
+D2 = n1 // n2
+P = n1 ** n2
+print(f'A soma é {S},a subtração é {SB}')
+print(f'A multiplicação é {M},e a divisão é {D}')
+print(f'O resto da divisão é {D2},e a potência é {P} ')
+print('Até mais',n0,'punheteiro XD ')
